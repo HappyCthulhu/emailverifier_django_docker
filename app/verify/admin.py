195c4adc2e5c435372648d518ffbddcf6f32176f
Валерий Газаров <v.gazarov@fortus.pro>
2022-08-13 19:18:52 +0400

    Add SendMessageAdmin class
    That is admin class for sending messages to emails

--- app/verify/admin.py
+++ app/verify/admin.py
@@ -1,8 +1,13 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 from django.contrib import admin
 from django.db.models import QuerySet
 
 from app.verify.clients import MillionVerifierClient
 from app.verify.models import Verify
+from config.settings.dev import EMAIL_SENDER
 from . import models
 
 
@@ -52,3 +57,37 @@ class VerifyAdmin(admin.ModelAdmin):
             Verify.objects.update(
                 result_code=code_response
             )
+
+
+@admin.register(models.Message)
+class SendMessageAdmin(admin.ModelAdmin):
+    model = models.Message
+    list_display = (
+        'message_recipient', 'message_was_sent', 'message_text',
+        'modified', 'created'
+    )
+
+    actions = ['send_message']
+
+    @admin.action(description='Отправить сообщение')
+    def send_message(modeladmin, request, qs: QuerySet):
+        msg = MIMEMultipart()
+
+        for message in qs:
+            msg['From'] = EMAIL_SENDER['email']
+            message_recipient = message.message_recipient.email
+            msg['To'] = message_recipient
+            msg['Subject'] = "Subscription"
+
+            text = message.message_text
+            msg.attach(MIMEText(text, 'plain'))
+
+            server = smtplib.SMTP_SSL('smtp.mail.ru', 465)
+            server.login(msg['From'], EMAIL_SENDER['password'])
+            server.sendmail(msg['From'], msg['To'], msg.as_string())
+            server.quit()
+
+            message.message_was_sent = True
+            message.save()
+
+            print(f"Successfully sent email to: {msg['To']}")
